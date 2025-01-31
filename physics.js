9cb7fbc58d7ab4629e34cc54c421a80208ec8d98
Helen Wu <wuz420@newschool.edu>
2025-01-31 01:10:55 -0500

    Updated

---
+++ physics.js
@@ -0,0 +1,32 @@
+// module aliases
+let Engine = Matter.Engine,
+    Render = Matter.Render,
+    Runner = Matter.Runner,
+    Bodies = Matter.Bodies,
+    Composite = Matter.Composite;
+
+// create an engine
+let engine = Engine.create();
+
+// create a renderer
+let render = Render.create({
+    element: document.body,
+    engine: engine
+});
+
+// create two boxes and a ground
+let boxA = Bodies.rectangle(400, 200, 80, 80);
+let boxB = Bodies.rectangle(450, 50, 80, 80);
+let ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
+
+// add all of the bodies to the world
+Composite.add(engine.world, [boxA, boxB, ground]);
+
+// run the renderer
+Render.run(render);
+
+// create runner
+let runner = Runner.create();
+
+// run the engine
+Runner.run(runner, engine);
