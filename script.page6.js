79165f222d8bc258492761d86f6872ce96e72f91
Helen Wu <wuz420@newschool.edu>
2025-03-09 19:55:05 -0400

    Updated

--- script.page6.js
+++ script.page6.js
@@ -1,6 +1,8 @@
 const essayTitle = document.getElementById("essay-title");
 const essay = document.getElementById("text-container");
 
+const interview = document.getElementById("interview-container");
+
 
 essay.onscroll = function(e){
     if (essay.scrollTop > 0) {
@@ -15,9 +17,14 @@ essay.addEventListener("wheel", function(event) {
     essay.scrollTop += event.deltaY * 0.3; // Adjust multiplier to slow down/speed up
 }, { passive: false });
 
+interview.addEventListener("wheel", function(event) {
+    event.preventDefault();
+    interview.scrollTop += event.deltaY * 0.3;
+}, { passive: false });
+
 
 document.querySelectorAll('.footnote').forEach(footnote => {
-    footnote.onmouseenter = function(e) {
+    footnote.onmouseenter = function() {
         const refId = this.getAttribute('data-id');
         const reference = document.getElementById(refId);
         if (reference) {
@@ -26,7 +33,7 @@ document.querySelectorAll('.footnote').forEach(footnote => {
         }
     };
 
-    footnote.onmouseleave = function(e) {
+    footnote.onmouseleave = function() {
         const refId = this.getAttribute('data-id');
         const reference = document.getElementById(refId);
         if (reference) {
@@ -34,3 +41,34 @@ document.querySelectorAll('.footnote').forEach(footnote => {
         }
     };
 });
+
+
+document.querySelectorAll('.add2').forEach(imgEnlarge => {
+    imgEnlarge.onclick = function(e) {
+
+        event.stopPropagation();
+
+        let img = this.closest('div').querySelector('img');
+        img.classList.toggle('enlarged');
+    };
+});
+
+document.onclick = function(e) {
+    document.querySelectorAll('img').forEach(img => img.classList.remove('enlarged'));
+};
+
+const interview1 = document.getElementById("interview1");
+const openInterview1 = document.getElementById("a3");
+
+let isToggled = false;
+
+openInterview1.onclick = function(){
+    if (isToggled){
+        interview1.style.visibility = "hidden";
+    }else{
+        interview1.style.visibility = "visible";
+    }
+
+    isToggled = !isToggled;
+
+}
