e468b85a69e814fa0d39fdfcc2251f2b52bb58e2
Helen Wu <wuz420@newschool.edu>
2025-03-19 02:04:27 -0400

    Updated//changed name of corresponding file

--- script.about.js
+++ script.about.js
@@ -17,3 +17,24 @@ sections.forEach(section =>{
         cursor.textContent = "ABOUT";
     }
 });
+
+const next = document.getElementById("next");
+
+next.onmouseenter = function(){
+    cursor.textContent = "GO TO NEXT PAGE";
+}
+
+next.onmouseleave = function(){
+    cursor.textContent = "ABOUT";
+}
+
+
+function openPopup1(url, width, height) {
+    let left = (screen.width - width) / 2;
+    let top = (screen.height - height) / 2;
+    window.open(url, '_blank', `width=${width},height=${height},left=${left},top=${top}`);
+}
+
+function openPopup2(url) {
+    window.open(url, '_blank', `width=${screen.width},height=${screen.height},top=0,left=0`);
+}
