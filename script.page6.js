6d4131c5e08d66f0c82a0b48fad13647fabbd58c
Helen Wu <wuz420@newschool.edu>
2025-04-07 01:39:21 -0400

    Updated

--- script.page6.js
+++ script.page6.js
@@ -30,3 +30,24 @@ function moveDivs() {
 
 
 setInterval(moveDivs, 1000);
+
+
+
+document.querySelectorAll('.footnote').forEach(footnote => {
+    footnote.onmouseenter = function() {
+        const refId = this.getAttribute('data-id');
+        const reference = document.getElementById(refId);
+        if (reference) {
+            const rect = this.getBoundingClientRect();
+            reference.style.visibility = 'visible';
+        }
+    };
+
+    footnote.onmouseleave = function() {
+        const refId = this.getAttribute('data-id');
+        const reference = document.getElementById(refId);
+        if (reference) {
+            reference.style.visibility = 'hidden';
+        }
+    };
+});
