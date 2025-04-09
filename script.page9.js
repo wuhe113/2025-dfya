1b55095a2af438d77fc62039d13dc2bfd7a86d5e
Helen Wu <wuz420@newschool.edu>
2025-04-09 13:39:39 -0400

    Updated

--- script.page9.js
+++ script.page9.js
@@ -1,6 +1,8 @@
 const background = document.getElementById("background");
 const gradient = document.getElementById("gradient");
 
+const interviewLOGO = document.getElementById("interview-logo");
+
 
 window.onscroll = function (e) {
     if (window.scrollY > 2000) {
@@ -9,5 +11,11 @@ window.onscroll = function (e) {
     } else {
         background.style.opacity = "1";
         gradient.style.opacity = "1";
+    }
+
+    if (window.scrollY > 50){
+        interviewLOGO.style.transform = "translateX(100%)";
+    } else{
+        interviewLOGO.style.transform = "translateX(0)";
     }
   };
